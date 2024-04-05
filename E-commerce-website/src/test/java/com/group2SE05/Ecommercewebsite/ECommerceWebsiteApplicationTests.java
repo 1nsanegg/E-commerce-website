620601fb6b1f0f3f5bbc0d8970909a8dcb782ac0
1nsanegg <2001040218@s.hanu.edu.vn>
2024-04-05 19:34:33 +0700

    first commit

---
+++ E-commerce-website/src/test/java/com/group2SE05/Ecommercewebsite/ECommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2SE05.Ecommercewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
